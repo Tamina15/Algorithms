f13c2ec9f34a40ab9aa3227d67b2bba0b6f024f3
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2025-02-12 17:10:07 +0700

    Change to EDT invoke

--- CellularAutomata/src/main/java/com/tmn/cellularautomata/Main.java
+++ CellularAutomata/src/main/java/com/tmn/cellularautomata/Main.java
@@ -1,44 +1,63 @@
 package com.tmn.cellularautomata;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Consumer;
+import javax.swing.SwingUtilities;
+
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Frame f = new Frame();
-        Panel p = new Panel(500, 500);
-        f.add(p);
-        f.pack();
-        f.setLocationRelativeTo(null);
+    private static Frame f;
+    private static Panel p;
 
-        Thread drawThread = Thread.startVirtualThread(() -> {
+    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
+        SwingUtilities.invokeAndWait(() -> {
+            f = new Frame();
+            p = new Panel(800, 500);
+            f.add(p);
+            f.pack();
+            f.setLocationRelativeTo(null);
+        });
+
+        Thread.startVirtualThread(loop(f, 60, () -> {
+            SwingUtilities.invokeLater(() -> {
+                f.repaint();
+            });
+        }, (Integer cps) -> {
+            p.fps = cps;
+        }));
+
+        Thread.startVirtualThread(loop(f, 60, () -> {
+            p.update();
+        }, (Integer cps) -> {
+            p.ups = cps;
+        }
+        ));
+    }
+
+    public static Runnable loop(Frame f, int fps, Runnable task, Consumer<Integer> perSecondTask) {
+        return () -> {
             double delta = 0;
-            double interval = 1000000000 / 60;
+            double interval = 1000000000 / fps;
             long lastTime = System.nanoTime();
+            long timer = 0;
+            int count = 0, cps = 0;
             while (f.isEnabled()) {
                 long currentTime = System.nanoTime();
                 delta += (currentTime - lastTime) / interval;
+                timer += (currentTime - lastTime);
                 lastTime = currentTime;
                 if (delta >= 1) {
-                    f.repaint();
+                    task.run();
+                    count++;
                     delta--;
                 }
-            }
-        });
-        Thread updateThread = Thread.startVirtualThread(() -> {
-            double delta = 0;
-            double interval = 1000000000 / 60;
-            long lastTime = System.nanoTime();
-            while (f.isEnabled()) {
-                long currentTime = System.nanoTime();
-                delta += (currentTime - lastTime) / interval;
-                lastTime = currentTime;
-                if (delta >= 1) {
-                    p.update();
-                    delta--;
+                if (timer >= 1000000000) {
+                    cps = count;
+                    perSecondTask.accept(cps);
+                    count = 0;
+                    timer = 0;
                 }
             }
-        });
-        drawThread.join();
-        updateThread.join();
-        System.out.println("Main");
+        };
     }
 }
