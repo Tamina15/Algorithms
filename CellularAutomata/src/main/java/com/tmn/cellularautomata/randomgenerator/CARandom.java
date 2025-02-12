de5aea9997fe334db2c93ca2746dcf0c3576a072
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2025-02-12 16:59:19 +0700

    RNG based on Elementary Cellular Automata

---
+++ CellularAutomata/src/main/java/com/tmn/cellularautomata/randomgenerator/CARandom.java
@@ -0,0 +1,15 @@
+package com.tmn.cellularautomata.randomgenerator;
+
+import java.util.Random;
+
+public class CARandom extends Random{
+
+    public CARandom() {
+    }
+
+    @Override
+    protected int next(int bits) {
+        return super.next(bits); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
