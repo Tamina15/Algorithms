b4773607b176a55dae07db61e52336fba3ff531c
NhatTranMinh15 <Nhat.TranMinh@nashtechglobal.com>
2025-02-12 17:10:28 +0700

    Misc

--- CellularAutomata/src/main/java/com/tmn/cellularautomata/Panel.java
+++ CellularAutomata/src/main/java/com/tmn/cellularautomata/Panel.java
@@ -6,12 +6,16 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseWheelEvent;
+import java.awt.geom.AffineTransform;
 import java.util.Random;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class Panel extends JPanel {
@@ -21,6 +25,7 @@ public class Panel extends JPanel {
     private Point mousePt = new Point(0, 0);
     private double zoomFactor = 1;
     private int scale = 1;
+    int fps, ups;
 
     public Panel(int width, int height) {
         this.width = width;
@@ -41,10 +46,12 @@ public class Panel extends JPanel {
         this.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                int dx = ((e.getX() - mousePt.x) * scale);
-                int dy = ((e.getY() - mousePt.y) * scale);
-                mousePt.setLocation(e.getX(), e.getY());
-                origin.setLocation(origin.getX() + dx, origin.getY() + dy);
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    int dx = ((e.getX() - mousePt.x) * scale);
+                    int dy = ((e.getY() - mousePt.y) * scale);
+                    mousePt.setLocation(e.getX(), e.getY());
+                    origin.setLocation(origin.getX() + dx, origin.getY() + dy);
+                }
             }
         });
 
@@ -58,8 +65,24 @@ public class Panel extends JPanel {
                 zoomFactor /= 1.1;
             }
         });
-
+        
         init();
+        
+        JTextField textField = new JTextField();
+        textField.setPreferredSize(new Dimension(100, 30));
+        textField.setBackground(Color.white);
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                try {
+                    int p = Integer.parseInt(textField.getText());
+                    ca.restart(p);
+                    wca.restart(p);
+                } catch (NumberFormatException err) {
+                }
+            }
+        });
+        this.add(textField);
     }
 
     public Panel(Dimension d) {
@@ -71,30 +94,40 @@ public class Panel extends JPanel {
     }
 
     private void init() {
-        int length = 150;
+        int length = width / 2;
         int[] initital = new int[length];
         Random r = new Random();
-        for(int i=0;i<initital.length;i++){
+        for (int i = 0; i < initital.length; i++) {
             initital[i] = r.nextInt(length);
         }
 //        ca = new CellularAutomata(100, width, height);
-        ca = new CellularAutomata(length);
+//        ca = new CellularAutomata(length);
+        ca = new CellularAutomata(length, width / 2, height, initital);
+        wca = new WrappedCellularAutomata(length, width / 2, height, initital);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D g2d = (Graphics2D) g.create();
+        AffineTransform at = g2d.getTransform();
         g2d.scale(zoomFactor, zoomFactor);
         g2d.translate(origin.x, origin.y);
 
         // draw here
         ca.draw(g2d);
+        wca.draw(g2d, ca.width + 15, 0);
         ///////////////
-        g2d.dispose();
+
+        g2d.setColor(Color.red);
+        g2d.drawString("FPS: " + fps, 0, -10);
+        g2d.drawString("UPS: " + ups, 0, -20);
+
+        g2d.setTransform(at);
     }
 
     CellularAutomata ca;
+    WrappedCellularAutomata wca;
 
     int waitBefore = 0, waitAfter = -1, delay;
 
@@ -114,10 +147,6 @@ public class Panel extends JPanel {
             delay = 0;
         }
         ca.update();
-        ca.update();
-        ca.update();
-        ca.update();
-        ca.update();
+        wca.update();
     }
-
 }
